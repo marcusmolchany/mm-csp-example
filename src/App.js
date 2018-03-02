58014279c42f8a5242ed4fb4c3ea49547e73b76a
marcusmolchany <marcusmolchany@gmail.com>
2018-03-01 23:41:22 -0800

    check for web3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isCheckingForWeb3: true,
+    web3: null,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        web3: window.web3,
+        isCheckingForWeb3: false,
+      });
+    }, 2000);
+  }
+
   render() {
+    const {
+      isCheckingForWeb3,
+      web3,
+    } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +30,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {isCheckingForWeb3 && <p>Checking to see if <code>window.web3</code> exists...</p>}
+          {!isCheckingForWeb3 && web3 && <p>web3 was injected!</p>}
+          {!isCheckingForWeb3 && !web3 && <p>gah! web3 wasn't injected!</p>}
         </p>
       </div>
     );
